b655ce6b74a062060c600803e7a9852f8b577bbb
Jan Głogowski <janekglo27@gmail.com>
2025-03-10 21:46:02 +0100

    A lot of imporovements regarding gui and morse logic

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from PIL import Image, ImageTk
+from morse_logic import *
 
 #--------CONSTANTS--------# 
 BACKGROUND = '#242424'
 BUTTON_BACKGROUND = '#828282'
 FONT_NAME = 'Chicago'
-WINDOW_SIZE = "800x800"
+WINDOW_SIZE = "800x670"
 
 #--------CREATING GUI CLASS--------# 
 class Gui(Tk):
@@ -21,27 +23,43 @@ class Gui(Tk):
             'learn': Frame(self,bg=BACKGROUND)
         }
 
-        self.main()
+        self.show_main()
 
+#--------CLEANING FRAMES--------# 
     def frame_clear(self):
         for frame in self.frames.values():
             frame.grid_forget()
 
-    def frame_setup(self, frame):
-        frame.grid(column=0, row=0, sticky='nsew')
-        frame.grid_columnconfigure(0, weight=1)  
-        frame.grid_columnconfigure(1, weight=1)
-        frame.grid_columnconfigure(2, weight=1)
-
-    def main(self):
+#--------MAIN GUI FRAME--------# 
+    def show_main(self):
         self.frame_clear()
-        self.frame_setup(self.frames['main'])
 
-        main_label = Label(self.frames['main'],text='Morse Code Converter',
-                           font=(FONT_NAME,50,'bold'),
+        self.frames['main'].grid(column=0, row=0, sticky="nsew")
+        for i in range(3):
+            self.frames['main'].grid_columnconfigure(i, weight=1)  
+
+        app_name = Label(self.frames['main'],
+                         text='Morse Code Converter',
+                         font=(FONT_NAME,51,'italic','bold'),
+                         bg=BACKGROUND,
+                         fg='white',
+                         justify='left')
+        app_name.grid(column=0,row=0,padx=30,pady=(20,0),columnspan=3,sticky='nsew') 
+
+        img = Image.open('machine.jpg')
+        img = img.resize((750,300))
+        self.photo = ImageTk.PhotoImage(img)
+
+        image_label = Label(self.frames['main'], image=self.photo,bg=BACKGROUND)
+        image_label.grid(column=1,row=1,pady=10)
+
+        label = Label(self.frames['main'],text='Welcome to Morse Code Converter! Choose one:',
+                           font=(FONT_NAME,20),
                            bg=BACKGROUND,
-                           fg='white')
-        main_label.grid(column=0,row=0,columnspan=3,padx=30,pady=(60,20))
+                           fg='white',
+                           justify='left',
+                           width=40)
+        label.grid(column=0,row=2,padx=30,pady=20,columnspan=3)
 
         encode_button = Button(self.frames['main'],
                                text="Encode",
@@ -50,7 +68,7 @@ class Gui(Tk):
                                bg=BUTTON_BACKGROUND,
                                fg='white',
                                command=self.show_encode)
-        encode_button.grid(column=1,row=1)
+        encode_button.grid(column=1,row=3)
 
         decode_button = Button(self.frames['main'],
                                text="Decode",
@@ -59,7 +77,7 @@ class Gui(Tk):
                                bg=BUTTON_BACKGROUND,
                                fg='white',
                                command=self.show_decode)
-        decode_button.grid(column=1,row=2,pady=8)
+        decode_button.grid(column=1,row=4,pady=8)
 
         learn_button = Button(self.frames['main'],
                               text="Learn",
@@ -68,31 +86,58 @@ class Gui(Tk):
                               bg=BUTTON_BACKGROUND,
                               fg='white',
                               command=self.show_learn)
-        learn_button.grid(column=1,row=3)
+        learn_button.grid(column=1,row=5)
 
+#--------ENCODE GUI FRAME--------# 
     def show_encode(self):
         self.frame_clear()
-        self.frame_setup(self.frames['encode'])
 
-        self.frames['encode'].grid(column=0, row=0)
+        self.frames['encode'].grid(column=0, row=0, sticky="nsew")
+        for i in range(0,15):  
+            self.frames['encode'].grid_columnconfigure(i, weight=1,minsize=30)  
+            if i < 7:  
+                self.frames['encode'].grid_rowconfigure(i, weight=1,minsize=10)
 
-        label = Label(self.frames['encode'], text="Encode", font=("Arial", 16))
-        label.grid(column=0, row=0, pady=20)
+        self.create_keyboard()
 
+        back_button = Button(self.frames['encode'],
+                               text="Back",
+                               font=(FONT_NAME,10),
+                               width=45,
+                               bg=BUTTON_BACKGROUND,
+                               fg='white',
+                               command=self.show_main)
+        back_button.grid(column=1,row=7,columnspan=14,padx=5)
+
+#--------ENCODE KEYBOARD SETUP--------#
+    def create_keyboard(self):
+        alphabet_dict = MorseLogic().alphabet_dict
+        letters = list(alphabet_dict.keys())
+        codes = list(alphabet_dict.values())
+        column_n,row_n = 1,1
+
+        for i in range(len(letters)):
+            alphabet_button = Button(self.frames['encode'],
+                                     text=f'{letters[i].upper()}\n{codes[i]}',
+                                     font=(FONT_NAME,13),
+                                     width=5,
+                                     bg=BUTTON_BACKGROUND,
+                                     fg='white')
+            alphabet_button.grid(column=column_n, row=row_n, sticky='nsew',pady=1,padx=1)
+            column_n += 1
+
+            if column_n == 14:
+                column_n = 1
+                row_n += 1
+#--------DECODE GUI FRAME--------# 
     def show_decode(self):
         self.frame_clear()
         self.frame_setup(self.frames['decode'])
 
-        self.frames['decode'].grid(column=0, row=0)
-
-        label = Label(self.frames['decode'], text="Decode", font=("Arial", 16))
-        label.grid(column=0, row=0, pady=20)
-
+#--------LEARN MODE GUI FRAME--------# 
     def show_learn(self):
         self.frame_clear()
         self.frame_setup(self.frames['learn'])
-
-        self.frames['learn'].grid(column=0, row=0)
 
         label = Label(self.frames['learn'], text="Learn", font=("Arial", 16))
         label.grid(column=0, row=0, pady=20)
--- morse_logic.py
+++ morse_logic.py
@@ -1,19 +1,25 @@
 import pandas as pd
 
-#--------READING DATA--------# 
-alphabet = pd.read_csv('morse_phonetic_alphabet.csv')
-df_alphabet = pd.DataFrame(alphabet)
+class MorseLogic():
+    def __init__(self):
+        super().__init__()
+        self.alphabet_dict = self.create_alphabet_dict()
 
-alphabet_dict = {row.letter:row.code for (index, row) in df_alphabet.iterrows()}
+#--------READING DATA--------# 
+    def create_alphabet_dict(self):
+        alphabet = pd.read_csv('morse_phonetic_alphabet.csv')
+        df_alphabet = pd.DataFrame(alphabet)
+        alphabet_dict = {row.letter:row.code for _, row in df_alphabet.iterrows()}
+        return alphabet_dict
 
 #--------ENCODING DATA--------# 
-def generate_phonetic():
-    try:
-        user_word = input("enter a word: ").lower()
-        word_encoded = [alphabet_dict[letter] for letter in user_word]
-    except KeyError:
-        print("Sorry, only letters in the alphabet please.")
-        generate_phonetic()
-    else:
-        print(word_encoded)
-        return word_encoded
+    def generate_phonetic(self):
+        try:
+            user_word = input("enter a word: ").lower()
+            word_encoded = [self.alphabet_dict[letter] for letter in user_word]
+        except KeyError:
+            print("Sorry, only letters in the alphabet please.")
+            self.generate_phonetic()
+        else:
+            print(word_encoded)
+            return word_encoded
